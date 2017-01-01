611b830468c9e96c47de2606e9ed5ae0a9a12f8a
EriveltonAlandelon <eriveltonalandelon@gmail.com>
2017-01-01 07:37:23 -0200

    Testes

--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,75 @@
+'use strict';
 
-var CACHE = 'play';
+const PREFIX = 'play';
+const HASH = '424c85f60'; // Calculated when running `gulp`.
+const OFFLINE_CACHE = `${ PREFIX }-${ HASH }`;
+const OFFLINE_URL = '/offline/index.html';
 
- 
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-
- 
-  evt.waitUntil(precache());
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(OFFLINE_CACHE).then(function(cache) {
+      return cache.addAll([
+        OFFLINE_URL,
+        '/css/estilo.css',
+        '/img/logo.png',
+        '/img/loading.jpg',
+        '/img/loading-video.svg',
+        '/img/facebook.svg',
+        '/img/website.svg',
+        '/js/loadCSS.js',
+        '/js/cssrelpreload.js',
+        '/scripts/search.js',
+        '/js/turbolinks.min.js',
+        '/js/jquery.min.js',
+        '/js/echo.min.js',
+        '/js/remodal.min.js',
+        '/js/jquery-lang.js',
+        '/js/langpack/en.json',
+        '/js/jekyll-search.js'
+      ]);
+    })
+  );
 });
 
- 
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-
- 
-  evt.respondWith(fromCache(evt.request));
-
- 
-  evt.waitUntil(update(evt.request));
+self.addEventListener('activate', function(event) {
+  // Delete old asset caches.
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key != OFFLINE_CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
- 
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll([
-      './css/estilo.css',
-      './img/logo.png',
-      './img/loading.jpg',
-      './img/loading-video.svg',
-      './img/facebook.svg',
-      './img/website.svg',
-      './js/loadCSS.js',
-      './js/cssrelpreload.js',
-      './scripts/search.js',
-      './js/turbolinks.min.js',
-      './js/jquery.min.js',
-      './js/echo.min.js',
-      './js/remodal.min.js',
-      './js/jquery-lang.js',
-      './js/langpack/en.json',
-      './js/jekyll-search.js'
-    ]);
-  });
-}
+self.addEventListener('fetch', function(event) {
+  if (event.request.mode == 'navigate') {
+    console.log(`Handling fetch event for ${ event.request.url }`);
+    event.respondWith(
+      fetch(event.request).catch(function(exception) {
+        // The `catch` is only triggered if `fetch()` throws an exception,
+        // which most likely happens due to the server being unreachable.
+        console.error(
+          'Fetch failed; returning offline page instead.',
+          exception
+        );
+        return caches.open(OFFLINE_CACHE).then(function(cache) {
+          return cache.match(OFFLINE_URL);
+        });
+      })
+    );
+  } else {
+    // It’s not a request for an HTML document, but rather for a CSS or SVG
+    // file or whatever…
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
 
- 
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
- 
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
+});
